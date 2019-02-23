9c39d5bb86eec56acf90fe754765aaa0eb4fbcdb
Chirag Manwani <cmkmanwani@gmail.com>
2019-02-23 17:21:39 +0530

    Added function for calculating word probability per class, added predict function

--- Q2/a2.py
+++ Q2/a2.py
@@ -7,7 +7,7 @@ from pathlib import Path
 def create_count_dict(train_filename):
     review_iterator = utils.json_reader(train_filename)
     count_dict = {}
-    count_class = [0, 0, 0, 0, 0]
+    count_class = np.ones(5)
     for review in review_iterator:
         text = review['text']
         rating = int(review['stars'])
@@ -15,15 +15,26 @@ def create_count_dict(train_filename):
         processed_text = utils.getStemmedDocuments(text)
         for word in processed_text:
             if word not in count_dict:
-                count_dict[word] = [0, 0, 0, 0, 0]
+                count_dict[word] = np.ones(5)
             count_dict[word][rating-1] += 1
     return count_dict, count_class
 
-def calc_word_prob(count_dict, count_class):
+def calc_word_log_prob(count_dict, count_class):
     for word in count_dict.keys():
-        count_dict[word] = count_dict[word] / count_class
+        count_dict[word] = np.log(count_dict[word] / count_class)
     count_class /= np.sum(count_class)
+    count_class = np.log(count_class)
     return count_dict, count_class
+
+def predict_class(review_text, theta_j_y, theta_y):
+    processed_text = utils.getStemmedDocuments(review_text)
+    num_classes = theta_j_y.shape[0]
+    prob_class = np.ones(num_classes)
+    for rating in range(0, 5):
+        for word in processed_text:
+            prob_class[rating] *= theta_j_y[word][rating]
+        prob_class[rating] *= (theta_y/np.sum(theta_y))
+    print(prob_class)
 
 def main(train_filename, test_filname):
     count_dict = Path('pickle_count_dict')
@@ -34,9 +45,18 @@ def main(train_filename, test_filname):
         count_class = pickle.load(open('pickle_count_class', 'rb'))
     else:
         count_dict, count_class = create_count_dict(train_filename)
+        pickle.dump(count_dict, open('pickle_count_dict', 'wb'))
+        pickle.dump(count_class, open('pickle_count_class', 'wb'))
     
-    theta_j_y, theta_y = calc_word_prob(count_dict, count_class)
-    
+    theta_j_y = Path('pickle_theta_j')
+    theta_y = Path('pickle_theta_y')
+    if theta_j_y.is_file():
+        theta_j_y = pickle.load(open('pickle_theta_j', 'rb'))
+        theta_y = pickle.load(open('pickle_theta_y', 'rb'))
+    else:
+        theta_j_y, theta_y = calc_word_log_prob(count_dict, count_class)
+        pickle.dump(theta_j_y, open('pickle_theta_j', 'wb'))
+        pickle.dump(theta_y, open('pickle_theta_y', 'wb'))
 
 if __name__ == '__main__':
     train_filename = '../temp/A2/data/train.json'
