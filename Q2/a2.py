7eb3904df1b9c4ee5e35f2ed95af547b0466cf20
Chirag Manwani <cmkmanwani@gmail.com>
2019-02-23 12:37:00 +0530

    Completed create_count_dict function

--- Q2/a2.py
+++ Q2/a2.py
@@ -1,25 +1,28 @@
 import sys
 import nltk
+import pickle
 sys.path.append('../temp/A2/ass2_data/')
 import utils
 
-def create_dict_count(train_filename):
+def create_count_dict(train_filename):
     review_iterator = utils.json_reader(train_filename)
-    dict_count = {}
+    count_dict = {}
     for review in review_iterator:
         text = review['text']
         rating = int(review['stars'])
         processed_text = utils.getStemmedDocuments(text)
         for word in processed_text:
-    
-            break
-
+            if word not in count_dict:
+                count_dict[word] = [0, 0, 0, 0, 0]
+            count_dict[word][rating] += 1
+    return count_dict
 
 def main(train_filename, test_filname):
-    create_dict_count(train_filename)
+    count_dict = create_dict_count(train_filename)
 
 if __name__ == '__main__':
     train_filename = '../temp/A2/data/train.json'
-    test_filename = 'test.json'
+    test_filename = '../temp/A2/data/test.json'
     main(train_filename, test_filename)
+
 
