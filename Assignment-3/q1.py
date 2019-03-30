27e8b666b4ad70e24c838fe3fadb01cf0aaad67f
Chirag Manwani <cmkmanwani@gmail.com>
2019-03-30 12:55:10 +0530

    Added boilerplate code

---
+++ Assignment-3/q1.py
@@ -0,0 +1,52 @@
+import sys
+import pandas
+import utils
+
+
+def part_a(
+    train_filename,
+    test_filename,
+    val_filename
+):
+    df = pandas.read_csv(train_filename)
+    cols_to_binarize = ['X1', 'X5', 'X12', 'X13', 'X14', 'X15', 'X16', 'X17'
+                        , 'X18', 'X19', 'X20', 'X21', 'X22', 'X23']
+    df = utils.binarize_median(df, cols_to_binarize)
+
+
+def main(
+    train_filename,
+    test_filname,
+    val_filename,
+    part
+):
+    if part == 'a':
+        part_a(train_filename, test_filename, val_filename)
+    # elif part == 'b':
+    #     part_b(train_filename, test_filename)
+    # elif part == 'c':
+    #     part_c(train_filename, test_filename)
+    # elif part == 'd':
+    #     part_d(train_filename, test_filname)
+    # elif part == 'e':
+    #     part_e(train_filename, test_filname)
+    # elif part == 'f':
+    #     part_e(train_filename, test_filname)
+    # elif part == 'g':
+    #     part_g(train_filename, test_filname)
+    else:
+        print('Invalid question part. a-f Valid')
+        exit()
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 5:
+        print('Incorrect number of arguments. 3 expected.')
+        exit()
+
+    train_filename = args[1]
+    test_filename = args[2]
+    val_filename = args[3]
+    part = args[3]
+    main(train_filename, test_filename, val_filename, part)
---
+++ Assignment-3/utils.py
@@ -0,0 +1,12 @@
+def binarize_median(
+    df,
+    columns
+):
+    '''
+    Input- 
+        df- dataframe
+        columns- columns to binarize
+    Output-
+        p_df- processed dataframe
+    '''
+    return df
