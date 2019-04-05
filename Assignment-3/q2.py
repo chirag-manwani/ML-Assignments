2595f4b289810bb3b3f4d129d6b29d8dbde98fca
Chirag Manwani <cmkmanwani@gmail.com>
2019-04-05 21:35:09 +0530

    Conformed q2.py part-a according to instructions. Changed gitignore

--- Assignment-3/q2.py
+++ Assignment-3/q2.py
@@ -2,12 +2,15 @@ import sys
 import pandas
 import utils
 import numpy as np
+import pickle
 from NN import NN
 
 
 def part_a(
     train_filename,
-    test_filename
+    test_filename,
+    one_hot_train_filename,
+    one_hot_test_filename
 ):
     data = pandas.read_csv(train_filename, header=None)
     X_train = data.drop(10, axis=1).values
@@ -22,16 +25,30 @@ def part_a(
     X_train = utils.one_hot_encoder(X_train, cols_to_encode, col_values)
     Y_train = utils.one_hot_encoder(np.reshape(Y_train, (Y_train.shape[0], 1)),
                                     [0], [range(10)])
-    return X_train, Y_train
+
+    data = np.hstack((X_train, Y_train))
+    np.savetxt(one_hot_train_filename, data.astype(int), delimiter=',',
+               fmt='%d')
+
+    data = pandas.read_csv(test_filename, header=None)
+    X_test = data.drop(10, axis=1).values
+    Y_test = data[10].values
+
+    X_test = utils.one_hot_encoder(X_test, cols_to_encode, col_values)
+    Y_test = utils.one_hot_encoder(np.reshape(Y_test, (Y_test.shape[0], 1)),
+                                   [0], [range(10)])
+    data = np.hstack((X_test, Y_test))
+    np.savetxt(one_hot_test_filename, data.astype(int), delimiter=',',
+               fmt='%d')
 
 
 def part_b(
+    config_file,
     train_filename,
     test_filename
 ):
-    X_train, Y_train = part_a(train_filename, test_filename)
-    nn = NN(85, 10, [100, 50])
-    nn.fit(X_train, Y_train)
+    nn = NN(85, 10, [5])
+    nn.fit(X_train, Y_train, lr=1, epochs=500, batch_size=100)
 
 
 def part_c(
@@ -49,18 +66,17 @@ def part_d(
 
 
 def main(
-    train_filename,
-    test_filname,
-    part
+    args
 ):
+    part = args[-1]
     if part == 'a':
-        part_a(train_filename, test_filename)
+        part_a(args[0], args[1], args[2], args[3])
     elif part == 'b':
-        part_b(train_filename, test_filename)
+        part_b(args[0], test_filename)
     elif part == 'c':
-        part_c(train_filename, test_filename)
+        part_c(args[0], test_filename)
     elif part == 'd':
-        part_d(train_filename, test_filename)
+        part_d(args[0], test_filename)
     # elif part == 'e':
     #     part_e(train_filename, test_filname)
     # elif part == 'f':
@@ -73,11 +89,4 @@ def main(
 
 
 if __name__ == '__main__':
-    args = sys.argv
-    if len(args) != 4:
-        print('Incorrect number of arguments. 3 expected.')
-        exit()
-    train_filename = args[1]
-    test_filename = args[2]
-    part = args[3]
-    main(train_filename, test_filename, part)
+    main(sys.argv[1:])
